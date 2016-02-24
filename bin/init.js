a31e11e46aa955775e1dc8bf99b4357d3490bc95
Michael <michael.danielczok@gmail.com>
2016-02-24 12:56:13 +0100

    Issue #14 In Template Qui wurden keine Styles für Seitentyp Kontakt vorgenommen. Zurest wurden entsprechende Pakete und Plugins local installiert. Dabei ist ein Bug mit Kontaktformular (als Baustein) entdeckt und direkt gefixt. Alle Elemente der Kontaktformular von Seitentyp Kontat wurden entsprechend angepasst. Als Hintergrund der erforderlichen Felder wurde ein PNG Bild (Stern) gesetzt. Farben kommen von Templateeinstellungen.

--- bin/init.js
+++ bin/init.js
@@ -1,37 +1,37 @@
-
 require.config({
 
-    baseUrl : URL_BIN_DIR +'QUI',
+    baseUrl: URL_BIN_DIR + 'QUI',
 
-    paths : {
-        "qui"      : URL_OPT_DIR +'bin/qui/qui',
-        "locale"   : URL_VAR_DIR +'locale/bin',
-        "controls" : URL_BIN_DIR +'QUI/controls'
+    paths: {
+       "qui"     : URL_OPT_DIR + 'bin/qui/qui',
+       "locale"  : URL_VAR_DIR + 'locale/bin',
+       "controls": URL_BIN_DIR + 'QUI/controls'
     },
 
-    waitSeconds : 0,
-    catchError  : true,
+    waitSeconds: 0,
+    catchError : true,
 
-    map : {
-        '*': {
-            'css': URL_OPT_DIR +'bin/qui/qui/lib/css.js'
-        }
-    }
+    map: {
+       '*': {
+           'css': URL_OPT_DIR + 'bin/qui/qui/lib/css.js'
+       }
+   }
 });
 
 
-window.addEvent('domready', function()
-{
+window.addEvent('domready', function () {
     "use strict";
 
-    // load QUI
-    require(['qui/QUI'], function(QUI)
-    {
-        QUI.addEvent("onError", function(msg, url, linenumber)
-        {
-            console.error( msg );
-            console.error( url );
-            console.error( 'LineNo: '+ linenumber );
+    require(['Locale'].append(QUIQQER_LOCALE), function (QUILocale) {
+        QUILocale.setCurrent(QUIQQER_PROJECT.lang);
+
+        // load QUI
+        require(['qui/QUI'], function (QUI) {
+            QUI.addEvent("onError", function (msg, url, linenumber) {
+                console.error(msg);
+                console.error(url);
+                console.error('LineNo: ' + linenumber);
+            });
         });
     });
 });
