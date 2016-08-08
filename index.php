bbd9c9a46dfbb9ced54b7609498389a719efb69b
Michael <michael.danielczok@gmail.com>
2016-08-08 10:19:38 +0200

    refactor: body Class hinzugefügt

--- index.php
+++ index.php
@@ -117,3 +117,34 @@ $Engine->assign(array(
     'langText'            => $langText
 
 ));
+
+
+/**
+ * Body Class
+ */
+
+$bodyClass = '';
+
+switch ($Template->getLayoutType()) {
+    case 'layout/startpage':
+        $bodyClass = 'homepage';
+        break;
+
+    case 'layout/leftSidebar':
+        $bodyClass = 'left-sidebar';
+        break;
+
+    case 'layout/rightSidebar':
+        $bodyClass = 'right-sidebar';
+        break;
+
+    default:
+        $bodyClass = 'no-sidebar';
+}
+
+$Engine->assign('bodyClass', $bodyClass);
+
+$Engine->assign(
+    'typeClass',
+    'type-'. str_replace(array('/',':'), '-', $Site->getAttribute('type'))
+);
